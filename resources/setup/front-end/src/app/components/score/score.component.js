ef1dab194341f9c805ffa7eed32807c44d10220c
OlivierFch <foucher.olivier@dbmail.com>
2022-04-20 00:26:11 +0200

    Sixth commit
    Arrays & functional programming (game.component.js + score.component.js)
    Promises & Async/await (game.component.js)

--- resources/setup/front-end/src/app/components/score/score.component.js
+++ resources/setup/front-end/src/app/components/score/score.component.js
@@ -27,22 +27,22 @@
 
     // TODO Step 6: Move this method to utils.js
     function parseUrl() {
-        var url = window.location;
-        var query = url.href.split('?')[1] || '';
-        var delimiter = '&';
-        var result = {};
-
-        var parts = query
-            .split(delimiter);
-        // TODO Step 3.3: Use Array.map() & Array.reduce()
-        for (var i in parts) {
-            var item = parts[i];
-            var kv = item.split('=');
+        let url = window.location;
+        let query = url.href.split("?")[1] || "";
+        let delimiter = "&";
+        let result = {};
+    
+        let parts = query.split(delimiter);
+        
+        return parts
+          .map((items) => {
+            return items.split("=");
+        })
+          .reduce((result, kv) => {
             result[kv[0]] = kv[1];
-        }
-
-        return result;
-    }
+            return result;
+          }, {});
+      }
 
     // put component in global scope, to be runnable right from the HTML.
     // TODO Step 7 export ScoreComponent
